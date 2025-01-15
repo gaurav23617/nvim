393d3ded6c1692ae066986fd78438a3033d9abff
gaurav23617 <gaurav23617@gmail.com>
2025-01-15 21:54:33 +0530

    noice removed form lazy.lua and lualine styled

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -15,31 +15,51 @@ return {
 		local lualine = require("lualine")
 		local lazy_status = require("lazy.status") -- to configure lazy pending updates count
 
-		-- configure lualine with modified theme
+		-- Custom function to show command line content
+		local function cmdline_content()
+			if vim.fn.mode() == "c" then -- Check if in command-line mode
+				return ":" .. vim.fn.getcmdline() -- Get and display current command
+			end
+			return "" -- Return empty when not in command-line mode
+		end
+
+		-- configure lualine with command line integration
 		lualine.setup({
-			globalstatus = vim.o.laststatus == 3,
-			disabled_filetypes = { statusline = { "dashboard", "alpha", "ministarter", "snacks_dashboard" } },
+			options = {
+				icons_enabled = true,
+				theme = "auto",
+				component_separators = { left = "", right = "" },
+				section_separators = { left = "", right = "" },
+				disabled_filetypes = { statusline = { "dashboard", "alpha", "ministarter", "snacks_dashboard" } },
+				globalstatus = true, -- Enables a global statusline
+			},
 			sections = {
 				lualine_a = { "mode" },
-				lualine_b = { "branch" },
-
+				lualine_b = { "branch", "diff", "diagnostics" },
+				lualine_c = { { "filename", path = 1 } },
 				lualine_x = {
 					{
 						lazy_status.updates,
 						cond = lazy_status.has_updates,
 						color = { fg = "#ff9e64" },
 					},
+					cmdline_content, -- Add command-line content to lualine
 					{ "encoding" },
 					{ "fileformat" },
 					{ "filetype" },
 				},
-
-				-- lualine_a = {
-				-- 	{
-				-- 		"buffers",
-				-- 	},
-				-- },
+				lualine_y = { "progress" },
+				lualine_z = { "location" },
 			},
+			inactive_sections = {
+				lualine_a = {},
+				lualine_b = {},
+				lualine_c = { { "filename", path = 1 } },
+				lualine_x = { "location" },
+				lualine_y = {},
+				lualine_z = {},
+			},
+			extensions = { "quickfix", "fugitive" },
 		})
 	end,
 }
