f44e0b01df12e23faf0123896fa499a3a2989b5c
gaurav23617 <gaurav23617@gmail.com>
2025-02-02 16:53:33 +0530

    mini animation added

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,6 +1,7 @@
 return {
 	"nvim-lua/plenary.nvim", -- lua functions that many plugins use
 	"christoomey/vim-tmux-navigator", -- tmux & split window navigation
+	{ "echasnovski/mini.animate", version = false },
 	{
 		"onsails/lspkind.nvim",
 		lazy = true,
@@ -25,6 +26,30 @@ return {
 				Unit = "",
 			},
 			menu = {},
+		},
+	},
+	{
+		"echasnovski/mini.move",
+		version = false,
+		-- Module mappings. Use `''` (empty string) to disable one.
+		mappings = {
+			-- Move visual selection in Visual mode. Defaults are Alt (Meta) + hjkl.
+			left = "<M-h>",
+			right = "<M-l>",
+			down = "<M-j>",
+			up = "<M-k>",
+
+			-- Move current line in Normal mode
+			line_left = "<M-h>",
+			line_right = "<M-l>",
+			line_down = "<M-j>",
+			line_up = "<M-k>",
+		},
+
+		-- Options which control moving behavior
+		options = {
+			-- Automatically reindent selection during linewise vertical move
+			reindent_linewise = true,
 		},
 	},
 }
