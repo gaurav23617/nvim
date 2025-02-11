7493c50310b20da1e53dcd2351db496e561686fe
gaurav23617 <gaurav23617@gmail.com>
2025-02-12 04:39:54 +0530

    some changes done

--- lua/plugins/nvim-ufo.lua
+++ lua/plugins/nvim-ufo.lua
@@ -1,6 +1,6 @@
 return {
   "kevinhwang91/nvim-ufo",
-  dependencies = { "kevinhwang91/promise-async" },
+  dependencies = { "kevinhwang91/promise-async", lazy = true },
   event = "BufRead",
   keys = {
     {
@@ -31,6 +31,7 @@ return {
     vim.o.foldlevel = 99
     vim.o.foldlevelstart = 100
     vim.o.foldenable = true
+
     local handler = function(virtText, lnum, endLnum, width, truncate)
       local newVirtText = {}
       local suffix = (" 󰁂 %d "):format(endLnum - lnum)
@@ -47,7 +48,7 @@ return {
           local hlGroup = chunk[2]
           table.insert(newVirtText, { chunkText, hlGroup })
           chunkWidth = vim.fn.strdisplaywidth(chunkText)
-          -- str width returned from truncate() may less than 2nd argument, need padding
+          -- str width returned from truncate() may be less than 2nd argument, need padding
           if curWidth + chunkWidth < targetWidth then
             suffix = suffix .. (" "):rep(targetWidth - curWidth - chunkWidth)
           end
@@ -59,12 +60,44 @@ return {
       return newVirtText
     end
 
+    -- Integrate your custom provider_selector and opts
     require("ufo").setup({
       fold_virt_text_handler = handler,
       close_fold_kinds_for_ft = {
         default = { "imports", "comment" },
         json = { "array" },
         c = { "comment", "region" },
+      },
+      opts = {
+        preview = {
+          mappings = {
+            scrollB = "<C-B>",
+            scrollF = "<C-F>",
+            scrollU = "<C-U>",
+            scrollD = "<C-D>",
+          },
+        },
+        provider_selector = function(_, filetype, buftype)
+          local function handleFallbackException(bufnr, err, providerName)
+            if type(err) == "string" and err:match("UfoFallbackException") then
+              return require("ufo").getFolds(bufnr, providerName)
+            else
+              return require("promise").reject(err)
+            end
+          end
+
+          return (filetype == "" or buftype == "nofile") and "indent" -- only use indent until a file is opened
+            or function(bufnr)
+              return require("ufo")
+                .getFolds(bufnr, "lsp")
+                :catch(function(err)
+                  return handleFallbackException(bufnr, err, "treesitter")
+                end)
+                :catch(function(err)
+                  return handleFallbackException(bufnr, err, "indent")
+                end)
+            end
+        end,
       },
     })
   end,
