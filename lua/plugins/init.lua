a870ddc3667562a75b47f15eef01a7e09967a256
gaurav23617 <gaurav23617@gmail.com>
2025-02-12 04:24:00 +0530

    lazy loading added

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,116 +1,121 @@
 return {
-	"nvim-lua/plenary.nvim", -- lua functions that many plugins use
-	"christoomey/vim-tmux-navigator", -- tmux & split window navigation
-	{ "echasnovski/mini.animate", version = false },  
+  "nvim-lua/plenary.nvim", -- lua functions that many plugins use
+  "christoomey/vim-tmux-navigator", -- tmux & split window navigation
+  { "echasnovski/mini.animate", version = false },
   {
     "OXY2DEV/helpview.nvim",
-    lazy = false
+    lazy = false,
   },
-	{
-		"m4xshen/hardtime.nvim",
-		-- lazy = true,
-		dependencies = { "MunifTanjim/nui.nvim" },
-		opts = {},
-	},
-	
-	{
-		"toppair/peek.nvim",
-		event = { "VeryLazy" },
-		build = "deno task --quiet build:fast",
-		config = function()
-			require("peek").setup({
-				filetype = { "markdown", "conf" },
-			})
-			vim.api.nvim_create_user_command("PeekOpen", require("peek").open, {})
-			vim.api.nvim_create_user_command("PeekClose", require("peek").close, {})
-		end,
-	},
-	{
-		"lewis6991/hover.nvim",
-  event = { "InsertEnter" },
-		config = function()
-			require("hover").setup({
-				init = function()
-					-- Require providers
-					require("hover.providers.lsp")
-					require("hover.providers.gh")
-					require("hover.providers.gh_user")
-					require("hover.providers.fold_preview")
-					require("hover.providers.dictionary")
-				end,
-				preview_opts = {
-					border = "single",
-				},
-				preview_window = false,
-				title = true,
-				mouse_providers = {
-					"LSP",
-				},
-				mouse_delay = 1000,
-			})
+  {
+    "m4xshen/hardtime.nvim",
+    -- lazy = true,
+    dependencies = { "MunifTanjim/nui.nvim" },
+    opts = {},
+  },
 
-			-- Setup keymaps
-			vim.keymap.set("n", "K", require("hover").hover, { desc = "hover.nvim" })
-			vim.keymap.set("n", "gK", require("hover").hover_select, { desc = "hover.nvim (select)" })
-			vim.keymap.set("n", "<C-p>", function()
-				require("hover").hover_switch("previous")
-			end, { desc = "hover.nvim (previous source)" })
-			vim.keymap.set("n", "<C-n>", function()
-				require("hover").hover_switch("next")
-			end, { desc = "hover.nvim (next source)" })
+  {
+    "toppair/peek.nvim",
+    event = { "VeryLazy" },
+    build = "deno task --quiet build:fast",
+    config = function()
+      require("peek").setup({
+        filetype = { "markdown", "conf" },
+      })
+      vim.api.nvim_create_user_command("PeekOpen", require("peek").open, {})
+      vim.api.nvim_create_user_command("PeekClose", require("peek").close, {})
+    end,
+  },
+  {
+    "lewis6991/hover.nvim",
+    event = { "InsertEnter" },
+    config = function()
+      require("hover").setup({
+        init = function()
+          -- Require providers
+          require("hover.providers.lsp")
+          require("hover.providers.gh")
+          require("hover.providers.gh_user")
+          require("hover.providers.fold_preview")
+          require("hover.providers.dictionary")
+        end,
+        preview_opts = {
+          border = "single",
+        },
+        preview_window = false,
+        title = true,
+        mouse_providers = {
+          "LSP",
+        },
+        mouse_delay = 1000,
+      })
 
-			-- Mouse support
-			vim.keymap.set("n", "<MouseMove>", require("hover").hover_mouse, { desc = "hover.nvim (mouse)" })
-			vim.o.mousemoveevent = true
-		end,
-	},
+      -- Setup keymaps
+      vim.keymap.set("n", "K", require("hover").hover, { desc = "hover.nvim" })
+      vim.keymap.set("n", "gK", require("hover").hover_select, { desc = "hover.nvim (select)" })
+      vim.keymap.set("n", "<C-p>", function()
+        require("hover").hover_switch("previous")
+      end, { desc = "hover.nvim (previous source)" })
+      vim.keymap.set("n", "<C-n>", function()
+        require("hover").hover_switch("next")
+      end, { desc = "hover.nvim (next source)" })
 
-	{
-		"echasnovski/mini.move",
-		version = false,
-		config = function()
-			require("mini.move").setup({
-				-- Module mappings. Use `''` (empty string) to disable one.
-				mappings = {
-					-- Move visual selection in Visual mode. Defaults are Alt (Meta) + hjkl.
-					left = "<M-h>",
-					right = "<M-l>",
-					down = "<M-j>",
-					up = "<M-k>",
+      -- Mouse support
+      vim.keymap.set(
+        "n",
+        "<MouseMove>",
+        require("hover").hover_mouse,
+        { desc = "hover.nvim (mouse)" }
+      )
+      vim.o.mousemoveevent = true
+    end,
+  },
 
-					-- Move current line in Normal mode
-					line_left = "<M-h>",
-					line_right = "<M-l>",
-					line_down = "<M-j>",
-					line_up = "<M-k>",
-				},
+  {
+    "echasnovski/mini.move",
+    version = false,
+    config = function()
+      require("mini.move").setup({
+        -- Module mappings. Use `''` (empty string) to disable one.
+        mappings = {
+          -- Move visual selection in Visual mode. Defaults are Alt (Meta) + hjkl.
+          left = "<M-h>",
+          right = "<M-l>",
+          down = "<M-j>",
+          up = "<M-k>",
 
-				-- Options which control moving behavior
-				options = {
-					-- Automatically reindent selection during linewise vertical move
-					reindent_linewise = true,
-				},
-			})
-		end,
-	},
-	{
-		"iamcco/markdown-preview.nvim",
-    	event = { "BufReadPre", "BufNewFile" },
-		keys = {
-			{
-				"<leader>mp",
-				ft = "markdown",
-				"<cmd>MarkdownPreviewToggle<cr>",
-				desc = "Markdown Preview",
-			},
-		},
-	},
-	{
-		"MeanderingProgrammer/render-markdown.nvim",
-    	event = { "BufReadPre", "BufNewFile" },
-		dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" }, -- if you prefer nvim-web-devicons
-		---@module 'render-markdown'
-		---@type render.md.UserConfig
-		opts = {},
-	},
+          -- Move current line in Normal mode
+          line_left = "<M-h>",
+          line_right = "<M-l>",
+          line_down = "<M-j>",
+          line_up = "<M-k>",
+        },
+
+        -- Options which control moving behavior
+        options = {
+          -- Automatically reindent selection during linewise vertical move
+          reindent_linewise = true,
+        },
+      })
+    end,
+  },
+  {
+    "iamcco/markdown-preview.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    keys = {
+      {
+        "<leader>mp",
+        ft = "markdown",
+        "<cmd>MarkdownPreviewToggle<cr>",
+        desc = "Markdown Preview",
+      },
+    },
+  },
+  {
+    "MeanderingProgrammer/render-markdown.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" }, -- if you prefer nvim-web-devicons
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    opts = {},
+  },
 }
