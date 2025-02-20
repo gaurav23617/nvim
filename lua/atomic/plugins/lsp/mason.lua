5a150097296c8e444b1da58e60868d00df8cacf8
gaurav23617 <gaurav23617@gmail.com>
2025-02-20 06:32:32 +0530

    lsp added and error fixed

--- lua/atomic/plugins/lsp/mason.lua
+++ lua/atomic/plugins/lsp/mason.lua
@@ -4,7 +4,9 @@ return {
     "williamboman/mason-lspconfig.nvim",
     "WhoIsSethDaniel/mason-tool-installer.nvim",
   },
-  build = ":MasonUpdate",
+  build = function()
+    pcall(vim.cmd, "MasonUpdate") -- Fixed build issue
+  end,
   config = function()
     local mason = require("mason")
     local mason_lspconfig = require("mason-lspconfig")
@@ -26,8 +28,11 @@ return {
         "ts_ls",
         "html",
         "cssls",
+        "tailwindcss",
+        "svelte",
         "lua_ls",
         "graphql",
+        "emmet_ls",
         "prismals",
         "pyright",
       },
@@ -35,6 +40,7 @@ return {
 
     mason_tool_installer.setup({
       ensure_installed = {
+        "biome",
         "stylua",
         "shfmt",
         "prettier",
