e89effcb4230bad4696096f9a059e40722e3ed49
gaurav23617 <gaurav23617@gmail.com>
2025-02-20 06:34:54 +0530

    error fixed

--- lua/atomic/plugins/lsp/lspconfig.lua
+++ lua/atomic/plugins/lsp/lspconfig.lua
@@ -9,31 +9,14 @@ return {
   config = function()
     local lspconfig = require("lspconfig")
     local mason_lspconfig = require("mason-lspconfig")
-    local keymap = vim.keymap
+    local capabilities = vim.lsp.protocol.make_client_capabilities() -- Fix for nil value
 
-    -- local capabilities = require("blink.cmp").default_capabilities() -- Use Blink.cmp capabilities
-    local capabilities = vim.lsp.protocol.make_client_capabilities()
-
-    -- Default LSP setup
     local function setup_lsp(server)
       lspconfig[server].setup({
         capabilities = capabilities,
       })
     end
 
-    vim.api.nvim_create_autocmd("LspAttach", {
-      group = vim.api.nvim_create_augroup("UserLspConfig", {}),
-      callback = function(ev)
-        local opts = { buffer = ev.buf, silent = true }
-        keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts)
-        keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts)
-        keymap.set("n", "K", vim.lsp.buf.hover, opts)
-        keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
-        keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
-      end,
-    })
-
-    -- LSP server configurations
     mason_lspconfig.setup_handlers({
       function(server_name)
         setup_lsp(server_name)
