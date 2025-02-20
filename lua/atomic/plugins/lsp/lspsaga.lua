af5a1cfe50912a1129e6998d69a1af115c31e10b
gaurav23617 <gaurav23617@gmail.com>
2025-02-20 06:44:53 +0530

    sowing properly

--- lua/atomic/plugins/lsp/lspsaga.lua
+++ lua/atomic/plugins/lsp/lspsaga.lua
@@ -1,35 +1,41 @@
 return {
-	"nvimdev/lspsaga.nvim",
-	event = { "BufReadPre", "BufNewFile" },
-	opt = {
-		symbol_in_winbar = {
-			enable = true,
-			separator = "| ",
-			hide_keyword = true,
-			show_file = true,
-			folder_level = 2,
-			respect_root = false,
-			color_mode = true,
-		},
-		outline = {
-			win_position = "botright",
-			win_with = "",
-			win_width = 30,
-			show_detail = true,
-			auto_preview = true,
-			auto_refresh = true,
-			auto_close = true,
-			custom_sort = nil,
-			keys = {
-				jump = "o",
-				expand_collapse = "u",
-				quit = "q",
-			},
-		},
-	},
-	keys = {
-		{ "n", "gd", "<cmd>Lspsaga goto_definition<CR>", { noremap = true, silent = true } },
-		{ "n", "<leader>ln", "<cmd>Lspsaga rename ++project<CR>", { noremap = true, silent = true } },
-		{ "n", "K", "<cmd>Lspsaga hover_doc<CR>", { noremap = true, silent = true } },
-	},
+  "nvimdev/lspsaga.nvim",
+  event = { "BufReadPre", "BufNewFile" },
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter", -- optional
+    "nvim-tree/nvim-web-devicons", -- optional
+  },
+  config = function()
+    require("lspsaga").setup({
+      symbol_in_winbar = {
+        enable = true,
+        separator = "| ",
+        hide_keyword = true,
+        show_file = true,
+        folder_level = 2,
+        respect_root = false,
+        color_mode = true,
+      },
+      outline = {
+        win_position = "botright",
+        win_with = "",
+        win_width = 30,
+        show_detail = true,
+        auto_preview = true,
+        auto_refresh = true,
+        auto_close = true,
+        custom_sort = nil,
+        keys = {
+          jump = "o",
+          expand_collapse = "u",
+          quit = "q",
+        },
+      },
+    })
+
+    local keymap = vim.keymap.set
+    keymap("n", "gd", "<cmd>Lspsaga goto_definition<CR>")
+    keymap("n", "<leader>ln", "<cmd>Lspsaga rename ++project<CR>")
+    keymap("n", "K", "<cmd>Lspsaga hover_doc<CR>")
+  end,
 }
