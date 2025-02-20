1a4f67b8117f08bbc98df358b049392b32b0659c
gaurav23617 <gaurav23617@gmail.com>
2025-02-20 05:39:24 +0530

    nvim ui and functions plugins added

---
+++ lua/atomic/plugins/ui/colorsechem.lua
@@ -0,0 +1,53 @@
+return {
+	"catppuccin/nvim",
+	lazy = false, -- Load immediately
+	priority = 1000, -- Ensure it loads before other plugins
+	opts = {
+		flavour = "mocha", -- latte, frappe, macchiato, mocha
+		background = {
+			light = "latte",
+			dark = "mocha",
+		},
+		integrations = {
+			aerial = true,
+			alpha = true,
+			cmp = true,
+			dashboard = true,
+			flash = true,
+			fzf = true,
+			grug_far = true,
+			gitsigns = true,
+			headlines = true,
+			illuminate = true,
+			indent_blankline = { enabled = true },
+			leap = true,
+			lsp_trouble = true,
+			mason = true,
+			markdown = true,
+			mini = true,
+			native_lsp = {
+				enabled = true,
+				underlines = {
+					errors = { "undercurl" },
+					hints = { "undercurl" },
+					warnings = { "undercurl" },
+					information = { "undercurl" },
+				},
+			},
+			navic = { enabled = true, custom_bg = "lualine" },
+			neotest = true,
+			neotree = true,
+			noice = true,
+			notify = true,
+			semantic_tokens = true,
+			snacks = true,
+			telescope = true,
+			treesitter = true,
+			treesitter_context = true,
+			which_key = true,
+		},
+	},
+	config = function()
+		vim.cmd.colorscheme("catppuccin") -- Apply the colorscheme
+	end,
+}
---
+++ lua/atomic/plugins/ui/treesitter.lua
@@ -0,0 +1,78 @@
+return {
+	{
+		"folke/which-key.nvim",
+		opts = {
+			spec = {
+				{ "<BS>", desc = "Decrement Selection", mode = "x" },
+				{ "<c-space>", desc = "Increment Selection", mode = { "x", "n" } },
+			},
+		},
+	},
+	{
+
+		"nvim-treesitter/nvim-treesitter",
+		event = { "VeryLazy", "BufReadPre", "BufNewFile" },
+		build = ":TSUpdate",
+		dependencies = {
+			"windwp/nvim-ts-autotag",
+		},
+		init = function(plugin)
+			require("lazy.core.loader").add_to_rtp(plugin)
+			require("nvim-treesitter.query_predicates")
+		end,
+		cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
+		keys = {
+			{ "<c-space>", desc = "Increment Selection" },
+			{ "<bs>", desc = "Decrement Selection", mode = "x" },
+		},
+		config = function()
+			-- import nvim-treesitter plugin
+			local treesitter = require("nvim-treesitter.configs")
+
+			-- configure treesitter
+			treesitter.setup({ -- enable syntax highlighting
+				highlight = {
+					enable = true,
+				},
+				-- enable indentation
+				indent = { enable = true },
+				-- enable autotagging (w/ nvim-ts-autotag plugin)
+				autotag = {
+					enable = true,
+				},
+				-- ensure these language parsers are installed
+				ensure_installed = {
+					"json",
+					"javascript",
+					"typescript",
+					"tsx",
+					"yaml",
+					"html",
+					"css",
+					"prisma",
+					"markdown",
+					"markdown_inline",
+					"svelte",
+					"graphql",
+					"bash",
+					"lua",
+					"vim",
+					"dockerfile",
+					"gitignore",
+					"query",
+					"vimdoc",
+					"c",
+				},
+				incremental_selection = {
+					enable = true,
+					keymaps = {
+						init_selection = "<C-space>",
+						node_incremental = "<C-space>",
+						scope_incremental = false,
+						node_decremental = "<bs>",
+					},
+				},
+			})
+		end,
+	},
+}
