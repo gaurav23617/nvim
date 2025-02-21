d1186c78763fe8e409bc63782f03d12109e3cb3f
gaurav23617 <gaurav23617@gmail.com>
2025-02-21 21:35:13 +0530

    some changes done

--- lua/atomic/plugins/lsp/commnet.lua
+++ lua/atomic/plugins/lsp/commnet.lua
@@ -1,19 +1,42 @@
 return {
-  "numToStr/Comment.nvim",
-  event = { "BufReadPre", "BufNewFile" },
-  dependencies = {
-    "JoosepAlviste/nvim-ts-context-commentstring",
+  {
+    "numToStr/Comment.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    dependencies = {
+      "JoosepAlviste/nvim-ts-context-commentstring",
+    },
+    config = function()
+      -- import comment plugin safely
+      local comment = require("Comment")
+
+      local ts_context_commentstring = require("ts_context_commentstring.integrations.comment_nvim")
+
+      -- enable comment
+      comment.setup({
+        -- for commenting tsx, jsx, svelte, html files
+        pre_hook = ts_context_commentstring.create_pre_hook(),
+      })
+    end,
   },
-  config = function()
-    -- import comment plugin safely
-    local comment = require("Comment")
+  {
+    "folke/todo-comments.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      local todo_comments = require("todo-comments")
 
-    local ts_context_commentstring = require("ts_context_commentstring.integrations.comment_nvim")
+      -- set keymaps
+      local keymap = vim.keymap -- for conciseness
 
-    -- enable comment
-    comment.setup({
-      -- for commenting tsx, jsx, svelte, html files
-      pre_hook = ts_context_commentstring.create_pre_hook(),
-    })
-  end,
+      keymap.set("n", "]t", function()
+        todo_comments.jump_next()
+      end, { desc = "Next todo comment" })
+
+      keymap.set("n", "[t", function()
+        todo_comments.jump_prev()
+      end, { desc = "Previous todo comment" })
+
+      todo_comments.setup()
+    end,
+  },
 }
