97a476746fdf3a07c5cd7faff1f4055815eda19b
gaurav23617 <gaurav23617@gmail.com>
2025-02-21 22:19:48 +0530

    code clean

--- lua/atomic/plugins/ui/snacks.lua
+++ lua/atomic/plugins/ui/snacks.lua
@@ -6,34 +6,11 @@ return {
   opts = {
     bigfile = { enabled = true },
     dashboard = { enabled = true },
-    -- dashboard = { example = "advanced" },
+    toggle = {
+      enabled = true,
+    },
     image = {
-      formats = {
-        "png",
-        "jpg",
-        "jpeg",
-        "gif",
-        "bmp",
-        "webp",
-        "tiff",
-        "heic",
-        "avif",
-        "mp4",
-        "mov",
-        "avi",
-        "mkv",
-        "webm",
-      },
-      force = false, -- try displaying the image, even if the terminal does not support it
-      doc = {
-        enabled = true,
-        inline = true,
-        float = true,
-        max_width = 80,
-        max_height = 40,
-      },
-      img_dirs = { "img", "images", "assets", "static", "public", "media", "attachments" },
-      cache = vim.fn.stdpath("cache") .. "/snacks/image",
+      enabled = true,
     },
     explorer = { enabled = true },
     indent = { enabled = true },
@@ -48,11 +25,6 @@ return {
     scroll = { enabled = true },
     statuscolumn = { enabled = true },
     words = { enabled = true },
-    styles = {
-      notification = {
-        -- wo = { wrap = true } -- Wrap notifications
-      },
-    },
   },
   keys = {
     -- Top Pickers & Explorer
