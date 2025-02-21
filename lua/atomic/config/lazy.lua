6f02643cd75bd056a6efec510a28feb33c3f7039
gaurav23617 <gaurav23617@gmail.com>
2025-02-21 21:35:00 +0530

    new path added

--- lua/atomic/config/lazy.lua
+++ lua/atomic/config/lazy.lua
@@ -1,17 +1,18 @@
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-	if vim.v.shell_error ~= 0 then
-		vim.api.nvim_echo({
-			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-			{ out, "WarningMsg" },
-			{ "\nPress any key to exit..." },
-		}, true, {})
-		vim.fn.getchar()
-		os.exit(1)
-	end
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out =
+    vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -20,37 +21,39 @@ vim.g.maplocalleader = "\\"
 
 -- Setup lazy.nvim
 require("lazy").setup({
-	spec = { -- import your plugins
-		{
-			{
-				import = "atomic.plugins",
-			},
-			{
-				import = "atomic.plugins.lsp",
-			},
-			{
-				import = "atomic.plugins.git",
-			},
-			{
-				import = "atomic.plugins.ui",
-			},
-			{
-				import = "atomic.plugins.utils",
-			},
-		},
-	},
-	-- Configure any other settings here. See the documentation for more details.
-	-- colorscheme that will be used when installing plugins.
-	-- install = {
-	-- 	colorscheme = { "catppuccin" },
-	-- },
-	-- automatically check for plugin updates
-	checker = {
-		enabled = true,
-		notify = false,
-	},
-	change_detection = {
-		enabled = true,
-		notify = false,
-	},
+  spec = { -- import your plugins
+    {
+      {
+        import = "atomic.plugins",
+      },
+      {
+        import = "atomic.plugins.lsp",
+      },
+      {
+        import = "atomic.plugins.git",
+      },
+      {
+        import = "atomic.plugins.ui",
+      },
+      {
+        import = "atomic.plugins.ui.snacks",
+      },
+      {
+        import = "atomic.plugins.utils",
+      },
+    },
+  },
+  -- Configure any other settings here. See the documentation for more details.
+  install = {
+    colorscheme = { "catppuccin" },
+  },
+  -- automatically check for plugin updates
+  checker = {
+    enabled = true,
+    notify = false,
+  },
+  change_detection = {
+    enabled = true,
+    notify = false,
+  },
 })
