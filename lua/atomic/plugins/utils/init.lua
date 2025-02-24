e41977bf6abd17b2b552e6124216cc35a0f3e36b
gaurav23617 <gaurav23617@gmail.com>
2025-02-24 22:40:31 +0530

    some new plugins added

--- lua/atomic/plugins/utils/init.lua
+++ lua/atomic/plugins/utils/init.lua
@@ -1,9 +1,25 @@
 return {
-	{
-		"nvim-tree/nvim-web-devicons",
-	},
-	{
-
-		"nvim-lua/plenary.nvim",
-	},
+  {
+    "nvim-tree/nvim-web-devicons",
+  },
+  {
+    "nvim-lua/plenary.nvim",
+  },
+  {
+    "szw/vim-maximizer",
+    keys = {
+      { "<leader>sm", "<cmd>MaximizerToggle<CR>", desc = "Maximize/minimize a split" },
+    },
+  },
+  {
+    "brianhuster/live-preview.nvim",
+    cmd = "LivePreview",
+    require("livepreview.config").set({
+      port = 5500,
+      browser = "default",
+      dynamic_root = false,
+      sync_scroll = false,
+      picker = "",
+    }),
+  },
 }
