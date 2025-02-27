f243473b687bffe4eadfe3a953b2ca7720e8528f
gaurav23617 <gaurav23617@gmail.com>
2025-02-27 10:50:54 +0530

    some changes done

--- lua/atomic/plugins/lsp/mason.lua
+++ lua/atomic/plugins/lsp/mason.lua
@@ -1,27 +1,32 @@
 return {
   "williamboman/mason.nvim",
+  event = "BufReadPre",
   dependencies = {
     "williamboman/mason-lspconfig.nvim",
     "WhoIsSethDaniel/mason-tool-installer.nvim",
   },
   build = function()
-    pcall(vim.cmd, "MasonUpdate") -- Fixed build issue
+    pcall(vim.cmd, "MasonUpdate") -- Auto-update Mason
   end,
-  config = function()
+
+  opts = {
+    ui = {
+      icons = {
+        package_installed = "✓",
+        package_pending = "➜",
+        package_uninstalled = "✗",
+      },
+    },
+  },
+
+  config = function(_, opts)
     local mason = require("mason")
     local mason_lspconfig = require("mason-lspconfig")
     local mason_tool_installer = require("mason-tool-installer")
 
-    mason.setup({
-      ui = {
-        icons = {
-          package_installed = "✓",
-          package_pending = "➜",
-          package_uninstalled = "✗",
-        },
-      },
-    })
+    mason.setup(opts)
 
+    -- Configure Mason LSP
     mason_lspconfig.setup({
       ensure_installed = {
         "biome",
@@ -35,9 +40,12 @@ return {
         "emmet_ls",
         "prismals",
         "pyright",
+        "jsonls",
       },
+      automatic_installation = true,
     })
 
+    -- Install Additional Tools
     mason_tool_installer.setup({
       ensure_installed = {
         "biome",
@@ -47,5 +55,10 @@ return {
         "eslint_d",
       },
     })
+
+    -- REMOVE THIS TO PREVENT MASON UI FROM OPENING AUTOMATICALLY
+    -- vim.defer_fn(function()
+    --   require("mason.ui").open()
+    -- end, 100)
   end,
 }
