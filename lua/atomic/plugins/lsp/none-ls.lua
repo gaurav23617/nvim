a8508a749debec8306a66a72ea692e13d1f15749
gaurav23617 <gaurav23617@gmail.com>
2025-02-27 20:54:27 +0530

    adding some new functions

--- lua/atomic/plugins/lsp/none-ls.lua
+++ lua/atomic/plugins/lsp/none-ls.lua
@@ -4,14 +4,48 @@ return {
   dependencies = { "mason.nvim" },
   config = function()
     local nls = require("null-ls")
+    local utils = require("null-ls.utils")
 
-    require("null-ls").setup({
+    -- Register formatting similar to LazyVim
+    local function format(buf)
+      vim.lsp.buf.format({
+        bufnr = buf,
+        filter = function(client)
+          return client.name == "null-ls"
+        end,
+      })
+    end
+
+    -- Get available sources for formatting
+    local function get_sources(buf)
+      local ret = require("null-ls.sources").get_available(
+        vim.bo[buf].filetype,
+        "NULL_LS_FORMATTING"
+      ) or {}
+      return vim.tbl_map(function(source)
+        return source.name
+      end, ret)
+    end
+
+    -- Setup null-ls
+    nls.setup({
+      root_dir = utils.root_pattern(".null-ls-root", ".neoconf.json", "Makefile", ".git"),
       sources = {
         nls.builtins.formatting.stylua,
         nls.builtins.formatting.prettier,
         nls.builtins.formatting.shfmt,
         nls.builtins.diagnostics.eslint_d,
       },
+      on_attach = function(client, bufnr)
+        if client.supports_method("textDocument/formatting") then
+          vim.api.nvim_create_autocmd("BufWritePre", {
+            buffer = bufnr,
+            callback = function()
+              format(bufnr)
+            end,
+          })
+        end
+      end,
     })
   end,
 }
