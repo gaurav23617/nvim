3aceb2712998e43f9b5e4a0d190c329f241d574a
gaurav23617 <gaurav23617@gmail.com>
2025-02-27 20:56:10 +0530

    tmux nav added

--- lua/atomic/plugins/utils/init.lua
+++ lua/atomic/plugins/utils/init.lua
@@ -1,5 +1,23 @@
 return {
   {
+    "christoomey/vim-tmux-navigator",
+    cmd = {
+      "TmuxNavigateLeft",
+      "TmuxNavigateDown",
+      "TmuxNavigateUp",
+      "TmuxNavigateRight",
+      "TmuxNavigatePrevious",
+      "TmuxNavigatorProcessList",
+    },
+    keys = {
+      { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+      { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+      { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+      { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+      { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+    },
+  },
+  {
     "nvim-tree/nvim-web-devicons",
   },
   {
@@ -14,12 +32,12 @@ return {
   {
     "brianhuster/live-preview.nvim",
     cmd = "LivePreview",
-    require("livepreview.config").set({
+    opts = {
       port = 5500,
       browser = "default",
       dynamic_root = false,
       sync_scroll = false,
       picker = "",
-    }),
+    },
   },
 }
