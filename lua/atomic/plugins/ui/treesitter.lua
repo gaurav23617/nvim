0adb1605700bda8ec8afe6c7adb40a28748b3cb6
gaurav23617 <gaurav23617@gmail.com>
2025-02-27 08:05:04 +0530

    some changes done

--- lua/atomic/plugins/ui/treesitter.lua
+++ lua/atomic/plugins/ui/treesitter.lua
@@ -9,70 +9,108 @@ return {
     },
   },
   {
-
     "nvim-treesitter/nvim-treesitter",
-    event = { "VeryLazy", "BufReadPre", "BufNewFile" },
+    event = { "BufReadPost", "BufNewFile" }, -- Lazy load for faster startup
     build = ":TSUpdate",
     dependencies = {
-      "windwp/nvim-ts-autotag",
+      "windwp/nvim-ts-autotag", -- Auto-close & rename HTML tags
     },
+    opts_extend = { "ensure_installed" },
+    -- Initialize Treesitter Early to Prevent Lazy Load Issues
     init = function(plugin)
       require("lazy.core.loader").add_to_rtp(plugin)
       require("nvim-treesitter.query_predicates")
     end,
+
     cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
+
+    -- Keybindings for Incremental Selection
     keys = {
       { "<c-space>", desc = "Increment Selection" },
-      { "<bs>", desc = "Decrement Selection", mode = "x" },
+      { "<BS>", desc = "Decrement Selection", mode = "x" },
     },
-    config = function()
-      -- import nvim-treesitter plugin
-      local treesitter = require("nvim-treesitter.configs")
 
-      -- configure treesitter
-      treesitter.setup({ -- enable syntax highlighting
-        highlight = {
-          enable = true,
+    opts = {
+      highlight = {
+        enable = true,
+        disable = function(lang, buf)
+          -- Disable for large files (>100KB)
+          local max_filesize = 100 * 1024 -- 100 KB
+          local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+          if ok and stats and stats.size > max_filesize then
+            return true
+          end
+        end,
+      },
+
+      indent = {
+        enable = true,
+        disable = { "python", "yaml" },
+      },
+
+      autotag = {
+        enable = true,
+      },
+
+      fold = {
+        enable = true,
+      },
+
+      ensure_installed = {
+        "json",
+        "javascript",
+        "typescript",
+        "tsx",
+        "yaml",
+        "html",
+        "css",
+        "prisma",
+        "markdown",
+        "markdown_inline",
+        "svelte",
+        "graphql",
+        "bash",
+        "lua",
+        "vim",
+        "dockerfile",
+        "gitignore",
+        "query",
+        "vimdoc",
+        "c",
+        "regex",
+      },
+
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = "<c-space>",
+          node_incremental = "<c-space>",
+          scope_incremental = false,
+          node_decremental = "<BS>",
         },
-        -- enable indentation
-        indent = { enable = true },
-        -- enable autotagging (w/ nvim-ts-autotag plugin)
-        autotag = {
-          enable = true,
-        },
-        -- ensure these language parsers are installed
-        ensure_installed = {
-          "json",
-          "javascript",
-          "typescript",
-          "tsx",
-          "yaml",
-          "html",
-          "css",
-          "prisma",
-          "markdown",
-          "markdown_inline",
-          "svelte",
-          "graphql",
-          "bash",
-          "lua",
-          "vim",
-          "dockerfile",
-          "gitignore",
-          "query",
-          "vimdoc",
-          "c",
-        },
-        incremental_selection = {
-          enable = true,
-          keymaps = {
-            init_selection = "<C-space>",
-            node_incremental = "<C-space>",
-            scope_incremental = false,
-            node_decremental = "<bs>",
-          },
-        },
-      })
+      },
+    },
+    config = function(_, opts)
+      require("nvim-treesitter.configs").setup(opts)
+    end,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter-context",
+    event = { "VeryLazy", "BufReadPre", "BufNewFile" },
+    opts = function()
+      local tsc = require("treesitter-context")
+      Snacks.toggle({
+        name = "Treesitter Context",
+        get = tsc.enabled,
+        set = function(state)
+          if state then
+            tsc.enable()
+          else
+            tsc.disable()
+          end
+        end,
+      }):map("<leader>ut")
+      return { mode = "cursor", max_lines = 3 }
     end,
   },
 }
