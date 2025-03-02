3dfdf81afc46e6634a6c4603131c98f47a688b45
gaurav23617 <gaurav23617@gmail.com>
2025-03-02 13:39:07 +0530

    some code cleaning done

--- lua/atomic/plugins/ui/lualine.lua
+++ lua/atomic/plugins/ui/lualine.lua
@@ -3,48 +3,38 @@ return {
   event = "VeryLazy",
   dependencies = {
     { "AndreM222/copilot-lualine" },
-    { "folke/noice.nvim", opts = {} }, -- Noice plugin for enhancing the command-line UI
+    { "folke/noice.nvim", opts = {} },
   },
   init = function()
     vim.g.lualine_laststatus = vim.o.laststatus
     if vim.fn.argc(-1) > 0 then
-      -- set an empty statusline till lualine loads
       vim.o.statusline = " "
     else
-      -- hide the statusline on the starter page
       vim.o.laststatus = 0
     end
   end,
-  config = function()
-    local lualine = require("lualine")
-    local lazy_status = require("lazy.status") -- to configure lazy pending updates count
+  opts = function()
+    local lazy_status = require("lazy.status")
     local noice = require("noice")
 
     -- Function to display macro recording status
     local function macro_recording()
       local recording_register = vim.fn.reg_recording()
-      if recording_register ~= "" then
-        return "Recording @" .. recording_register
-      end
-      return ""
+      return recording_register ~= "" and ("Recording @" .. recording_register) or ""
     end
 
-    -- configure lualine with command-line integration via noice.nvim and macro recording status
-    lualine.setup({
+    return {
       options = {
         icons_enabled = true,
         theme = "auto",
         component_separators = { left = "", right = "" },
         section_separators = { left = "", right = "" },
         disabled_filetypes = { statusline = { "dashboard", "alpha", "ministarter" } },
-        globalstatus = true, -- Enables a global statusline
+        globalstatus = true,
       },
       sections = {
         lualine_a = {
-          {
-            "mode",
-            icon = " ",
-          },
+          { "mode", icon = " " },
         },
         lualine_b = {
           "branch",
@@ -67,11 +57,11 @@ return {
             color = { fg = "#cdd6f4" },
           },
           {
-            macro_recording, -- Displays macro recording status
+            macro_recording,
             cond = function()
               return vim.fn.reg_recording() ~= ""
             end,
-            color = { fg = "#ff0000" }, -- Highlight macro recording
+            color = { fg = "#ff0000" },
           },
           {
             lazy_status.updates,
@@ -80,12 +70,11 @@ return {
           },
           {
             "copilot",
-            -- Default values
             symbols = {
               status = {
                 icons = {
                   enabled = " ",
-                  sleep = " ", -- auto-trigger disabled
+                  sleep = " ",
                   disabled = " ",
                   warning = " ",
                   unknown = " ",
@@ -98,7 +87,7 @@ return {
                   unknown = "#FF5555",
                 },
               },
-              spinners = "dots", -- has some premade spinners
+              spinners = "dots",
               spinner_color = "#6272A4",
             },
             show_colors = false,
@@ -120,6 +109,6 @@ return {
         lualine_z = {},
       },
       extensions = { "quickfix", "fugitive" },
-    })
+    }
   end,
 }
