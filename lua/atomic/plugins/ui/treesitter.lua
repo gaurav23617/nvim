8747217dc413250916a5f1a1e477a543d8b787d3
gaurav23617 <gaurav23617@gmail.com>
2025-03-06 00:27:42 +0530

    comment removed

--- lua/atomic/plugins/ui/treesitter.lua
+++ lua/atomic/plugins/ui/treesitter.lua
@@ -39,7 +39,6 @@ return {
       highlight = {
         enable = true,
         disable = function(lang, buf)
-          -- Disable for large files (>100KB)
           local max_filesize = 100 * 1024 -- 100 KB
           local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
           if ok and stats and stats.size > max_filesize then
