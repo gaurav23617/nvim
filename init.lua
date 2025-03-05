d110d2513cf4851b62abe5a8cdc4d70771fef567
gaurav23617 <gaurav23617@gmail.com>
2025-03-06 00:31:14 +0530

    vim function removed

--- init.lua
+++ init.lua
@@ -1,12 +1,5 @@
-require("atomic.config.lazy")
 require("atomic.config.options")
+require("atomic.config.settings")
+require("atomic.config.lazy")
 require("atomic.config.keymaps")
 require("atomic.config.auto-cmd")
-
-vim.g.loaded_matchparen = 1
-vim.g.skip_loading_ftplugin = 1
-vim.cmd("filetype plugin indent off")
-vim.loader.enable()
-vim.g.python3_host_prog = "~/.venvs/nvim/bin/python"
-vim.o.sessionoptions =
-  "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
