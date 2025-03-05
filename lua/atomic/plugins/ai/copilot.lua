9349308071bc0bb3898c7d0836a2ad26c6b51efe
gaurav23617 <gaurav23617@gmail.com>
2025-03-06 00:29:09 +0530

    copilot code changed

--- lua/atomic/plugins/ai/copilot.lua
+++ lua/atomic/plugins/ai/copilot.lua
@@ -1,51 +1,46 @@
 return {
-  "zbirenbaum/copilot.lua",
-  cmd = "Copilot",
-  event = "InsertEnter",
-  config = function()
-    require("copilot").setup({
+  {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    build = ":Copilot auth",
+    event = "InsertEnter",
+    opts = {
       panel = {
         enabled = true,
-        auto_refresh = false,
-        keymap = {
-          jump_prev = "[[",
-          jump_next = "]]",
-          accept = "<CR>",
-          refresh = "gr",
-          open = "<M-CR>",
-        },
-        layout = {
-          position = "bottom", -- | top | left | right | horizontal | vertical
-          ratio = 0.4,
-        },
       },
       suggestion = {
         enabled = true,
-        auto_trigger = false,
-        hide_during_completion = true,
-        debounce = 75,
-        keymap = {
-          accept = "<M-l>",
-          accept_word = false,
-          accept_line = false,
-          next = "<M-]>",
-          prev = "<M-[>",
-          dismiss = "<C-]>",
-        },
       },
       filetypes = {
-        yaml = false,
-        markdown = false,
-        help = false,
-        gitcommit = false,
-        gitrebase = false,
-        hgcommit = false,
-        svn = false,
-        cvs = false,
-        ["."] = false,
+        markdown = true,
+        help = true,
+        lua = true,
+        javascript = true,
+        typescript = true,
+        python = true,
+        css = true,
+        html = true,
       },
-      copilot_node_command = "node", -- Node.js version must be > 18.x
-      server_opts_overrides = {},
-    })
-  end,
+      auto_start = true,
+    },
+    config = function(_, opts)
+      require("copilot").setup(opts)
+
+      -- AI Accept Action
+      vim.api.nvim_create_user_command("CopilotAccept", function()
+        local copilot = require("copilot.suggestion")
+        if copilot.is_visible() then
+          copilot.accept()
+        end
+      end, {})
+
+      -- Map Copilot Accept to <M-l>
+      vim.keymap.set("i", "<M-l>", function()
+        local copilot = require("copilot.suggestion")
+        if copilot.is_visible() then
+          copilot.accept()
+        end
+      end, { desc = "Accept Copilot Suggestion" })
+    end,
+  },
 }
