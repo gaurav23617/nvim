ee5dae37b80c3b5a9dc9461d5392ca099771d0e4
gaurav23617 <gaurav23617@gmail.com>
2025-03-24 12:32:20 +0530

    lualine added

--- lua/plugins/ui/lualine.lua
+++ lua/plugins/ui/lualine.lua
@@ -44,6 +44,7 @@ return {
           },
           { "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
           { LazyVim.lualine.pretty_path() },
+          { "pipeline", icon = "" },
         },
         lualine_x = {
           Snacks.profiler.status(),
