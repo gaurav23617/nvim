b465b2c574a0ad80dbae3de00b756ab45a8e5e93
gaurav23617 <gaurav23617@gmail.com>
2025-04-20 13:14:04 +0530

    code formated

--- lua/plugins/blink-cmp.lua
+++ lua/plugins/blink-cmp.lua
@@ -17,10 +17,10 @@ return {
           max_items = 8,
           min_keyword_length = 3,
           opts = {
-            dictionary_directories = { vim.fn.expand("~/.config/nvim/dictionaries") },
+            dictionary_directories = { vim.fn.expand "~/.config/nvim/dictionaries" },
             dictionary_files = {
-              vim.fn.expand("~/.config/nvim/spell/en.utf-8.add"),
-              vim.fn.expand("~/.config/nvim/spell/es.utf-8.add"),
+              vim.fn.expand "~/.config/nvim/spell/en.utf-8.add",
+              vim.fn.expand "~/.config/nvim/spell/es.utf-8.add",
             },
           },
         },
