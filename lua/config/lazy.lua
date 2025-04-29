066d33d8c48918cacbcc4b1aa5cb7ce75179ae77
Gaurav <138373404+gaurav23617@users.noreply.github.com>
2025-04-29 02:58:01 +0100

    Update lazy.lua

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -1,9 +1,7 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = "https://github.com/folke/lazy.nvim.git"
   local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-  local out = vim.fn.system { "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath }
   if vim.v.shell_error ~= 0 then
     vim.api.nvim_echo({
       { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
@@ -17,16 +15,16 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-require("lazy").setup {
   spec = {
     -- add LazyVim and import its plugins
-    {
+        {
       "gaurav23617/LazyVim",
       import = "lazyvim.plugins",
       opts = {
         colorscheme = "catppuccin",
       },
     },
+    -- import/override with your plugins
     { import = "plugins" },
   },
   defaults = {
@@ -38,19 +36,12 @@ require("lazy").setup {
     version = false, -- always use the latest git commit
     -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
-  local_spec = true,
-  install = {
-    colorscheme = { "catppuccin" },
-  },
+  install = { colorscheme = { "catppuccin", "tokyonight" } },
   checker = {
     enabled = true, -- check for plugin updates periodically
     notify = false, -- notify on update
   }, -- automatically check for plugin updates
   performance = {
-    cache = {
-      enabled = true,
-      -- disable_events = {},
-    },
     rtp = {
       -- disable some rtp plugins
       disabled_plugins = {
@@ -65,4 +56,4 @@ require("lazy").setup {
       },
     },
   },
-}
+})
