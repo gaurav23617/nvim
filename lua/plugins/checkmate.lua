a7552b70c61a54092b4b1709bd165db1cc6ac199
gaurav23617 <gaurav23617@gmail.com>
2025-06-15 19:38:11 +0530

    testing new plugins

---
+++ lua/plugins/checkmate.lua
@@ -0,0 +1,9 @@
+return {
+  "bngarren/checkmate.nvim",
+  ft = "markdown", -- Lazy loads for Markdown files matching patterns in 'files'
+  version = "~0.8.0", -- pins to minor 0.8.x
+  opts = {
+    -- your configuration here
+    -- or leave empty to use defaults
+  },
+}
