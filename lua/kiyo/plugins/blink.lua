2cbb930c9c85fd10fd7a05911d01249d80f5aeb8
gaurav23617 <gaurav23617@gmail.com>
2025-08-08 19:25:45 +0530

    blink config changedd

--- lua/kiyo/plugins/blink.lua
+++ lua/kiyo/plugins/blink.lua
@@ -1,92 +1,77 @@
 return {
-  { "L3MON4D3/LuaSnip", keys = {} },
-  {
-    "saghen/blink.cmp",
-    dependencies = {
-      "rafamadriz/friendly-snippets",
-    },
-    -- event = "InsertEnter",
-    version = "*",
-    config = function()
-      -- vim.cmd('highlight Pmenu guibg=none')
-      -- vim.cmd('highlight PmenuExtra guibg=none')
-      -- vim.cmd('highlight FloatBorder guibg=none')
-      -- vim.cmd('highlight NormalFloat guibg=none')
-
-      require("blink.cmp").setup({
-        snippets = { preset = "luasnip" },
-        signature = { enabled = true },
-        appearance = {
-          use_nvim_cmp_as_default = false,
-          nerd_font_variant = "normal",
-        },
-        sources = {
-          -- per_filetype = {
-          --     codecompanion = { "codecompanion" },
-          -- },
-          default = { "laravel", "lazydev", "lsp", "path", "snippets", "buffer" },
-          providers = {
-            lazydev = {
-              name = "LazyDev",
-              module = "lazydev.integrations.blink",
-              score_offset = 100,
-            },
-            -- laravel = {
-            --     name = "Laravel",
-            --     module = "laravel.blink_source",
-            --     enabled = function()
-            --         return vim.bo.filetype == 'php' or vim.bo.filetype == 'blade'
-            --     end,
-            --     kind = "Laravel",
-            --     score_offset = 1000, -- Highest priority
-            --     min_keyword_length = 1,
-            -- },
-            laravel = {
-              name = "laravel",
-              module = "laravel.blink_source",
-            },
-            cmdline = {
-              min_keyword_length = 2,
-            },
-          },
-        },
-        keymap = {
-          ["<C-f>"] = {},
-        },
-        cmdline = {
-          enabled = false,
-          completion = { menu = { auto_show = true } },
-          keymap = {
-            ["<CR>"] = { "accept_and_enter", "fallback" },
-          },
-        },
-        completion = {
-          menu = {
-            border = nil,
-            scrolloff = 1,
-            scrollbar = false,
-            draw = {
-              columns = {
-                { "kind_icon" },
-                { "label", "label_description", gap = 1 },
-                { "kind" },
-                { "source_name" },
-              },
-            },
-          },
-          documentation = {
-            window = {
-              border = nil,
-              scrollbar = false,
-              winhighlight = "Normal:BlinkCmpDoc,FloatBorder:BlinkCmpDocBorder,EndOfBuffer:BlinkCmpDoc",
-            },
-            auto_show = true,
-            auto_show_delay_ms = 500,
-          },
-        },
-      })
-
-      require("luasnip.loaders.from_vscode").lazy_load()
-    end,
+  "saghen/blink.cmp",
+  -- event = { "InsertEnter" },
+  version = "*",
+  opts_extend = {
+    "sources.completion.enabled_providers",
+    "sources.compat",
+    "sources.default",
   },
+  dependencies = {
+    "rafamadriz/friendly-snippets",
+    {
+      "L3MON4D3/LuaSnip",
+      config = function()
+        require("luasnip.loaders.from_snipmate").lazy_load({ paths = { "./luasnip_snippets" } })
+      end,
+    },
+    {
+      "saghen/blink.compat",
+      optional = true, -- make optional so it's only enabled if any extras need it
+      version = "*",
+      opts = {},
+    },
+    "neovim/nvim-lspconfig",
+    "Kaiser-Yang/blink-cmp-dictionary",
+    "folke/lazydev.nvim",
+    "fang2hou/blink-copilot",
+    "mikavilpas/blink-ripgrep.nvim",
+    "folke/snacks.nvim",
+    {
+      "mei28/blink-bang-word-light.nvim",
+      event = { "VeryLazy" },
+      opts = {},
+    },
+  },
+  opts = {
+    fuzzy = {},
+    keymap = {},
+    appearance = {},
+    completion = {},
+    cmdline = {},
+    term = {},
+    snippets = {},
+    sources = {},
+  },
+  config = function(_, opts)
+    -- Ensure compatibility sources are enabled
+    local enabled = opts.sources.default or {}
+    for _, source in ipairs(opts.sources.compat or {}) do
+      opts.sources.providers[source] = vim.tbl_deep_extend(
+        "force",
+        { name = source, module = "blink.compat.source" },
+        opts.sources.providers[source] or {}
+      )
+      if type(enabled) == "table" and not vim.tbl_contains(enabled, source) then
+        table.insert(enabled, source)
+      end
+    end
+
+    -- Add AI accept to <Tab> key
+    if not opts.keymap["<Tab>"] then
+      if opts.keymap.preset == "super-tab" then
+        opts.keymap["<Tab>"] = {
+          require("blink.cmp.keymap.presets")["super-tab"]["<Tab>"][1],
+          { "snippet_forward", "ai_accept" },
+          "fallback",
+        }
+      else
+        opts.keymap["<Tab>"] = {
+          { "snippet_forward", "ai_accept" },
+          "fallback",
+        }
+      end
+    end
+    require("blink.cmp").setup(opts)
+  end,
 }
