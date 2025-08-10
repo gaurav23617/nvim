e24b488c92b759dcda2807e03ddce927831facdd
gaurav23617 <gaurav23617@gmail.com>
2025-08-10 22:16:09 +0530

    keymap change

--- lua/kiyo/plugins/noice.lua
+++ lua/kiyo/plugins/noice.lua
@@ -3,7 +3,7 @@ return {
   event = "VeryLazy",
   dependencies = {
     "MunifTanjim/nui.nvim",
-    "rcarriga/nvim-notify",
+    -- "rcarriga/nvim-notify",
   },
   opts = {
     lsp = {
@@ -40,8 +40,8 @@ return {
       { "<leader>nh", function() require("noice").cmd("history") end, desc = "Noice History" },
       { "<leader>na", function() require("noice").cmd("all") end, desc = "Noice All" },
       { "<leader>nd", function() require("noice").cmd("dismiss") end, desc = "Dismiss All" },
-      { "<leader>nt", function() require("noice").cmd("pick") end, desc = "Noice Picker (Telescope/FzfLua)" },
-      { "<c-f>", function() if not require("noice.lsp").scroll(4) then return "<c-f>" end end, silent = true, expr = true, desc = "Scroll Forward", mode = {"i", "n", "s"} },
+      { "<leader>nt", function() require("noice").cmd("pick") end, desc = "Noice Picker" },
+      -- { "<c-f>", function() if not require("noice.lsp").scroll(4) then return "<c-f>" end end, silent = true, expr = true, desc = "Scroll Forward", mode = {"i", "n", "s"} },
       { "<c-b>", function() if not require("noice.lsp").scroll(-4) then return "<c-b>" end end, silent = true, expr = true, desc = "Scroll Backward", mode = {"i", "n", "s"}},
     },
   config = function(_, opts)
