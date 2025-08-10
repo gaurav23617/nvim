0f4df3e906e5cf3e211d63557a4bda9fbf258b83
gaurav23617 <gaurav23617@gmail.com>
2025-08-10 22:15:49 +0530

    event cha nge to bufreafpre

--- lua/kiyo/plugins/git.lua
+++ lua/kiyo/plugins/git.lua
@@ -1,7 +1,7 @@
 return {
   {
     "lewis6991/gitsigns.nvim",
-    event = "VeryLazy",
+    event = "BufReadPre",
     config = function()
       -- local icons = require('config.icons')
       require("gitsigns").setup({
