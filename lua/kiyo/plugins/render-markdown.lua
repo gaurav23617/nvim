8f4a7fa1913a92ad3b0386e66f39cf110e14f7a8
gaurav23617 <gaurav23617@gmail.com>
2025-09-05 02:08:34 +0530

    heading color changed

--- lua/kiyo/plugins/render-markdown.lua
+++ lua/kiyo/plugins/render-markdown.lua
@@ -9,82 +9,70 @@ return {
     ---@module 'render-markdown'
     ft = { "markdown", "norg", "rmd", "org" },
     opts = {
+      -- Enable rendering in all modes
+      render_modes = { "n", "c", "t" }, -- normal, command, terminal (exclude visual 'v' and insert 'i')
+
       heading = {
-        sign = false,
-        icons = { "󰎤 ", "󰎧 ", "󰎪 ", "󰎭 ", "󰎱 ", "󰎳 " },
-      },
-      code = {
-        sign = false,
-        width = "block",
-        right_pad = 1,
-      },
-      bullet = {
-        -- Turn on / off list bullet rendering
         enabled = true,
-      },
-      checkbox = {
-        -- Turn on / off checkbox state rendering
-        enabled = true,
-        -- Determines how icons fill the available space:
-        --  inline:  underlying text is concealed resulting in a left aligned icon
-        --  overlay: result is left padded with spaces to hide any additional text
-        position = "inline",
-        unchecked = {
-          -- Replaces '[ ]' of 'task_list_marker_unchecked'
-          icon = "   󰄱 ",
-          -- Highlight for the unchecked icon
-          highlight = "RenderMarkdownUnchecked",
-          -- Highlight for item associated with unchecked checkbox
-          scope_highlight = nil,
+        sign = true,
+        width = "full",
+        -- Heading icons
+        icons = { "󰲡 ", "󰲣 ", "󰲥 ", "󰲧 ", "󰲩 ", "󰲫 " },
+        -- Background highlight groups
+        backgrounds = {
+          "RenderMarkdownH1Bg",
+          "RenderMarkdownH2Bg",
+          "RenderMarkdownH3Bg",
+          "RenderMarkdownH4Bg",
+          "RenderMarkdownH5Bg",
+          "RenderMarkdownH6Bg",
         },
-        checked = {
-          -- Replaces '[x]' of 'task_list_marker_checked'
-          icon = "   󰱒 ",
-          -- Highlight for the checked icon
-          highlight = "RenderMarkdownChecked",
-          -- Highlight for item associated with checked checkbox
-          scope_highlight = nil,
+        -- Foreground highlight groups
+        foregrounds = {
+          "RenderMarkdownH1",
+          "RenderMarkdownH2",
+          "RenderMarkdownH3",
+          "RenderMarkdownH4",
+          "RenderMarkdownH5",
+          "RenderMarkdownH6",
         },
       },
     },
+    config = function(_, opts)
+      require("render-markdown").setup(opts)
+
+      -- Set up highlight groups with white text and default backgrounds
+      vim.api.nvim_set_hl(0, "RenderMarkdownH1", { fg = "#ffffff", bold = true })
+      vim.api.nvim_set_hl(0, "RenderMarkdownH1Bg", { fg = "#ffffff" })
+
+      vim.api.nvim_set_hl(0, "RenderMarkdownH2", { fg = "#ffffff", bold = true })
+      vim.api.nvim_set_hl(0, "RenderMarkdownH2Bg", { fg = "#ffffff" })
+
+      vim.api.nvim_set_hl(0, "RenderMarkdownH3", { fg = "#ffffff", bold = true })
+      vim.api.nvim_set_hl(0, "RenderMarkdownH3Bg", { fg = "#ffffff" })
+
+      vim.api.nvim_set_hl(0, "RenderMarkdownH4", { fg = "#ffffff", bold = true })
+      vim.api.nvim_set_hl(0, "RenderMarkdownH4Bg", { fg = "#ffffff" })
+
+      vim.api.nvim_set_hl(0, "RenderMarkdownH5", { fg = "#ffffff", bold = true })
+      vim.api.nvim_set_hl(0, "RenderMarkdownH5Bg", { fg = "#ffffff" })
+
+      vim.api.nvim_set_hl(0, "RenderMarkdownH6", { fg = "#ffffff", bold = true })
+      vim.api.nvim_set_hl(0, "RenderMarkdownH6Bg", { fg = "#ffffff" })
+
+      -- Try to increase font size for GUI Neovim clients
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = { "markdown", "norg", "rmd", "org" },
+        callback = function()
+          -- For GUI clients that support font sizing
+          if vim.g.neovide then
+            -- Neovide specific font scaling
+            vim.api.nvim_set_hl(0, "RenderMarkdownH1Bg", { fg = "#ffffff", bold = true })
+            vim.api.nvim_set_hl(0, "RenderMarkdownH2Bg", { fg = "#ffffff", bold = true })
+            vim.api.nvim_set_hl(0, "RenderMarkdownH3Bg", { fg = "#ffffff", bold = true })
+          end
+        end,
+      })
+    end,
   },
-  -- Render Markdown Inline
-  -- {
-  --   "MeanderingProgrammer/render-markdown.nvim",
-  --   ft = { "markdown", "norg", "rmd", "org", "codecompanion" },
-  --   opts = {
-  --     code = {
-  --       sign = false,
-  --       width = "block",
-  --       right_pad = 1,
-  --     },
-  --     heading = {
-  --       sign = false,
-  --       icons = {},
-  --     },
-  --     checkbox = {
-  --       enabled = false,
-  --     },
-  --   },
-  --   config = function(_, opts)
-  --     require("render-markdown")
-  --       .setup(opts)
-  --       -- require("languages")
-  --       .toggle({
-  --         name = "Render Markdown",
-  --         get = function()
-  --           return require("render-markdown.state").enabled
-  --         end,
-  --         set = function(enabled)
-  --           local m = require("render-markdown")
-  --           if enabled then
-  --             m.enable()
-  --           else
-  --             m.disable()
-  --           end
-  --         end,
-  --       })
-  --       :map("<leader>um")
-  --   end,
-  -- },
 }
