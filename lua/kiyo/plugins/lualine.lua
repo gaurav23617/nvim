985c1d4238e15e1d246e7c13cdc2749a2cb43a0a
gaurav23617 <gaurav23617@gmail.com>
2025-09-21 09:10:45 +0530

    lualine ui changed
    Signed-off-by: gaurav23617 <gaurav23617@gmail.com>

--- lua/kiyo/plugins/lualine.lua
+++ lua/kiyo/plugins/lualine.lua
@@ -1,177 +1,250 @@
 return {
   "nvim-lualine/lualine.nvim",
-  event = "VeryLazy",
-  dependencies = {
-    { "AndreM222/copilot-lualine" },
-    { "folke/noice.nvim", opts = {} },
-  },
-  init = function()
-    vim.g.lualine_laststatus = vim.o.laststatus
-    if vim.fn.argc(-1) > 0 then
-      vim.o.statusline = " "
-    else
-      vim.o.laststatus = 0
-    end
-  end,
-  opts = function()
-    -- Function to get the root directory name with a fixed color
-    local function root_dir()
+  opts = function(_, opts)
+    local auto = require("lualine.themes.auto")
+
+    local colors = {
+      rosewater = "#f2d5cf",
+      flamingo = "#eebebe",
+      pink = "#f4b8e4",
+      mauve = "#ca9ee6",
+      red = "#e78284",
+      maroon = "#ea999c",
+      peach = "#ef9f76",
+      yellow = "#e5c890",
+      green = "#a6d189",
+      teal = "#81c8be",
+      sky = "#99d1db",
+      sapphire = "#85c1dc",
+      blue = "#8caaee",
+      lavender = "#babbf1",
+      text = "#c6d0f5",
+      subtext1 = "#b5bfe2",
+      subtext0 = "#a5adce",
+      overlay2 = "#949cbb",
+      overlay1 = "#838ba7",
+      overlay0 = "#737994",
+      surface2 = "#626880",
+      surface1 = "#51576d",
+      surface0 = "#414559",
+      base = "#303446",
+      mantle = "#292c3c",
+      crust = "#232634",
+    }
+
+    local function separator()
       return {
         function()
-          local cwd = vim.fn.getcwd()
-          return "󱉭 " .. vim.fn.fnamemodify(cwd, ":t")
+          return "│"
         end,
-        color = { fg = "#16bc40" },
+        color = { fg = colors.surface0, bg = "NONE", gui = "bold" },
+        padding = { left = 1, right = 1 },
       }
     end
 
-    -- Function to format file paths nicely
-    local function pretty_path()
-      return function()
-        local path = vim.fn.expand("%:p")
-        if path == "" then
-          return ""
-        end
-
-        local cwd = vim.fn.getcwd()
-        if path:find(cwd, 1, true) == 1 then
-          path = path:sub(#cwd + 2)
-        end
-
-        local parts = vim.split(path, "[\\/]")
-        if #parts > 3 then
-          parts = { parts[1], "…", unpack(parts, #parts - 2, #parts) }
-        end
-
-        return table.concat(parts, "/")
+    local function custom_branch()
+      local gitsigns = vim.b.gitsigns_head
+      local fugitive = vim.fn.exists("*FugitiveHead") == 1 and vim.fn.FugitiveHead() or ""
+      local branch = gitsigns or fugitive
+      if branch == nil or branch == "" then
+        return ""
+      else
+        return " " .. branch
       end
     end
 
-    -- Ensure lazy.status exists before calling it
-    local lazy_updates = function()
-      local ok, lazy_status = pcall(require, "lazy.status")
-      return ok and lazy_status.updates() or ""
-    end
-    local lazy_has_updates = function()
-      local ok, lazy_status = pcall(require, "lazy.status")
-      return ok and lazy_status.has_updates()
-    end
-
-    local lazy_status = require("lazy.status")
-    local noice = require("noice")
-
-    -- Function to display macro recording status
-    local function macro_recording()
-      local recording_register = vim.fn.reg_recording()
-      return recording_register ~= "" and ("Recording @" .. recording_register) or ""
-    end
-
-    -- Function to get Copilot status
-    local function copilot_status()
-      if not package.loaded["copilot"] then
-        return " "
+    local modes = { "normal", "insert", "visual", "replace", "command", "inactive", "terminal" }
+    for _, mode in ipairs(modes) do
+      if auto[mode] and auto[mode].c then
+        auto[mode].c.bg = "NONE"
       end
-
-      local clients = vim.lsp.get_clients({ name = "copilot" })
-      if #clients == 0 then
-        return " "
-      end
-
-      local copilot = require("copilot.api").status.data
-      if copilot and copilot.status then
-        local status = copilot.status
-        local icons = {
-          ["InProgress"] = " ", -- Spinner
-          ["Warning"] = " ", -- Warning
-          ["Error"] = " ", -- Error
-          ["Unknown"] = " ", -- Unknown
-          ["Normal"] = " ", -- Active
-        }
-        return icons[status]
-      end
-      return " "
     end
 
-    return {
-      options = {
-        icons_enabled = true,
-        theme = "auto",
-        component_separators = { left = "", right = "" },
-        section_separators = { left = "", right = "" },
-        disabled_filetypes = { statusline = { "dashboard", "alpha", "ministarter" } },
-        globalstatus = true,
-      },
-      sections = {
-        lualine_a = {
-          { "mode", icon = " " },
+    opts.options = vim.tbl_deep_extend("force", opts.options or {}, {
+      theme = auto,
+      component_separators = "",
+      section_separators = "",
+      globalstatus = true,
+      disabled_filetypes = { statusline = {}, winbar = {} },
+    })
+
+    opts.sections = {
+      lualine_a = {
+        {
+          "mode",
+          fmt = function(str)
+            return str:sub(1, 1)
+          end,
+          color = function()
+            local mode = vim.fn.mode()
+            if mode == "\22" then
+              return { fg = "none", bg = colors.red, gui = "bold" }
+            elseif mode == "V" then
+              return { fg = colors.red, bg = "none", gui = "underline,bold" }
+            else
+              return { fg = colors.red, bg = "none", gui = "bold" }
+            end
+          end,
+          padding = { left = 0, right = 0 },
         },
-        lualine_b = {
-          "branch",
+      },
+      lualine_b = {
+        separator(),
+        {
+          custom_branch,
+          color = { fg = colors.green, bg = "none", gui = "bold" },
+          padding = { left = 0, right = 0 },
+        },
+        {
           "diff",
-          {
-            "diagnostics",
-            sources = { "nvim_diagnostic" },
-            symbols = { error = " ", warn = " ", info = " ", hint = " " },
+          colored = true,
+          diff_color = {
+            added = { fg = colors.teal, bg = "none", gui = "bold" },
+            modified = { fg = colors.yellow, bg = "none", gui = "bold" },
+            removed = { fg = colors.red, bg = "none", gui = "bold" },
           },
+          symbols = { added = "+", modified = "~", removed = "-" },
+          source = nil,
+          padding = { left = 1, right = 0 },
         },
-        lualine_c = {
-          root_dir(),
-          { "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
-          { pretty_path() },
+      },
+      lualine_c = {
+        separator(),
+        {
+          "filetype",
+          icon_only = true,
+          colored = false,
+          color = { fg = colors.blue, bg = "none", gui = "bold" },
+          padding = { left = 0, right = 1 },
         },
-        lualine_x = {
-          {
-            function()
-              return noice.api.status.command.get()
-            end,
-            cond = function()
-              return noice.api.status.command.has()
-            end,
-            color = { fg = "#cdd6f4" },
+        {
+          "filename",
+          file_status = true,
+          path = 0,
+          shorting_target = 20,
+          symbols = {
+            modified = "[+]",
+            readonly = "[-]",
+            unnamed = "[?]",
+            newfile = "[!]",
           },
-          {
-            macro_recording,
-            cond = function()
-              return vim.fn.reg_recording() ~= ""
-            end,
-            color = { fg = "#ff0000" },
-          },
-          {
-            lazy_updates,
-            cond = lazy_has_updates,
-            color = { fg = "#ff9e64" },
-          },
-          {
-            copilot_status,
-            color = function()
-              local copilot = package.loaded["copilot"] and require("copilot.api").status.data
-              if copilot and copilot.status then
-                local status = copilot.status
-                return {
-                  fg = (status == "InProgress" and "#FFB86C")
-                    or (status == "Warning" and "#FF5555")
-                    or "#50FA7B",
-                }
+          color = { fg = colors.blue, bg = "none", gui = "bold" },
+          padding = { left = 0, right = 0 },
+        },
+        separator(),
+        {
+          function()
+            local bufnr_list = vim.fn.getbufinfo({ buflisted = 1 })
+            local total = #bufnr_list
+            local current_bufnr = vim.api.nvim_get_current_buf()
+            local current_index = 0
+
+            for i, buf in ipairs(bufnr_list) do
+              if buf.bufnr == current_bufnr then
+                current_index = i
+                break
               end
-              return { fg = "#6272A4" }
+            end
+
+            return string.format(" %d/%d", current_index, total)
+          end,
+          color = { fg = colors.yellow, bg = "none", gui = "bold" },
+          padding = { left = 0, right = 0 },
+        },
+      },
+      lualine_x = {
+        {
+          "fileformat",
+          color = { fg = colors.yellow, bg = "none", gui = "bold" },
+          symbols = {
+            unix = "",
+            dos = "",
+            mac = "",
+          },
+          padding = { left = 0, right = 0 },
+        },
+        {
+          "encoding",
+          color = { fg = colors.yellow, bg = "none", gui = "bold" },
+          padding = { left = 1, right = 0 },
+        },
+        separator(),
+        {
+          function()
+            local size = vim.fn.getfsize(vim.api.nvim_buf_get_name(0))
+            if size < 0 then
+              return "-"
+            else
+              if size < 1024 then
+                return size .. "B"
+              elseif size < 1024 * 1024 then
+                return string.format("%.1fK", size / 1024)
+              elseif size < 1024 * 1024 * 1024 then
+                return string.format("%.1fM", size / (1024 * 1024))
+              else
+                return string.format("%.1fG", size / (1024 * 1024 * 1024))
+              end
+            end
+          end,
+          color = { fg = colors.blue, bg = "none", gui = "bold" },
+          padding = { left = 0, right = 0 },
+        },
+      },
+      lualine_y = {
+        separator(),
+        {
+          "diagnostics",
+          sources = { "nvim_diagnostic", "coc" },
+          sections = { "error", "warn", "info", "hint" },
+          diagnostics_color = {
+            error = function()
+              local count = #vim.diagnostic.get(0, { severity = vim.diagnostic.severity.ERROR })
+              return { fg = (count == 0) and colors.green or colors.red, bg = "none", gui = "bold" }
+            end,
+            warn = function()
+              local count = #vim.diagnostic.get(0, { severity = vim.diagnostic.severity.WARN })
+              return {
+                fg = (count == 0) and colors.green or colors.yellow,
+                bg = "none",
+                gui = "bold",
+              }
+            end,
+            info = function()
+              local count = #vim.diagnostic.get(0, { severity = vim.diagnostic.severity.INFO })
+              return { fg = (count == 0) and colors.green or colors.blue, bg = "none", gui = "bold" }
+            end,
+            hint = function()
+              local count = #vim.diagnostic.get(0, { severity = vim.diagnostic.severity.HINT })
+              return { fg = (count == 0) and colors.green or colors.teal, bg = "none", gui = "bold" }
             end,
           },
-          { "encoding" },
-          { "fileformat" },
-          { "filetype" },
+          symbols = {
+            error = "󰅚 ",
+            warn = "󰀪 ",
+            info = "󰋽 ",
+            hint = "󰌶 ",
+          },
+          colored = true,
+          update_in_insert = false,
+          always_visible = true,
+          padding = { left = 0, right = 0 },
         },
-        lualine_y = { "progress" },
-        lualine_z = { "location" },
       },
-      inactive_sections = {
-        lualine_a = {},
-        lualine_b = {},
-        lualine_c = { { "filename", path = 1 } },
-        lualine_x = { "location" },
-        lualine_y = {},
-        lualine_z = {},
+      lualine_z = {
+        separator(),
+        {
+          "progress",
+          color = { fg = colors.red, bg = "none", gui = "bold" },
+          padding = { left = 0, right = 0 },
+        },
+        {
+          "location",
+          color = { fg = colors.red, bg = "none", gui = "bold" },
+          padding = { left = 1, right = 0 },
+        },
       },
-      extensions = { "neo-tree", "lazy", "fzf", "quickfix", "fugitive" },
     }
+
+    return opts
   end,
 }
