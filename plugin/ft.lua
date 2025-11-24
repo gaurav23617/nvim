c1ed3f5c4c9833a17683f7f406d515fb5450d192
gaurav23617 <gaurav23617@gmail.com>
2025-11-24 23:43:04 +0530

    feat: some more pattern added for dotenv file

--- plugin/ft.lua
+++ plugin/ft.lua
@@ -1,9 +1,17 @@
 -- if a file is a .env or .envrc file, set the filetype to sh
 vim.filetype.add({
+  extension = {
+    env = "dotenv",
+  },
   filename = {
     [".env"] = "sh",
     [".envrc"] = "sh",
     ["*.env"] = "sh",
     ["*.envrc"] = "sh",
+    ["env"] = "dotenv",
+  },
+  pattern = {
+    ["[jt]sconfig.*.json"] = "jsonc",
+    ["%.env%.[%w_.-]+"] = "dotenv",
   },
 })
