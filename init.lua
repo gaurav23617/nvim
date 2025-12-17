d279c0e57042b54244e6f07bafdaa9223d2518b8
gaurav2361 <gaurav23617@gmail.com>
2025-12-17 20:46:43 +0530

    feat: creating nvim new config

--- init.lua
+++ init.lua
@@ -1,9 +1,6 @@
-require("kiyo.core.mason-path")
-require("kiyo.config.options")
-require("kiyo.core.lazy")
-require("kiyo.config.keymaps")
-require("kiyo.config.autocmds")
-require("kiyo.config.lsp-utils")
-require("kiyo.config.mason-verify")
-require("kiyo.config.health-check")
-require("kiyo.core.lsp")
+vim.g.mapleader = " " -- change leader to a space
+vim.g.maplocalleader = " " -- change localleader to a space
+vim.opt.clipboard = "unnamedplus" -- allows neovim to access the system clipboard
+vim.opt.number = true -- set numbered lines
+vim.opt.smartcase = true -- smart case
+vim.opt.smartindent = true -- make indenting smarter again
--- lsp/prismals.lua
+++
@@ -1,8 +0,0 @@
-local blink = require("blink.cmp")
-
----@type vim.lsp.Config
-return {
-  cmd = { "prisma-language-server", "--stdio" },
-  root_markers = { ".git", "package.json" },
-  filetypes = { "prisma" },
-}
