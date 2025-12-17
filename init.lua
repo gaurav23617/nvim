7f8f2c8bc9253da34b3991b07d67839c2ca49a3d
gaurav2361 <gaurav23617@gmail.com>
2025-12-17 22:29:18 +0530

    feat: lazy.nvim added and colorscheme added

--- init.lua
+++ init.lua
@@ -2,5 +2,8 @@ vim.g.mapleader = " " -- change leader to a space
 vim.g.maplocalleader = " " -- change localleader to a space
 vim.opt.clipboard = "unnamedplus" -- allows neovim to access the system clipboard
 vim.opt.number = true -- set numbered lines
+vim.opt.relativenumber = true -- set relative numbered lines
 vim.opt.smartcase = true -- smart case
 vim.opt.smartindent = true -- make indenting smarter again
+
+require("kiyo.core.lazy")
