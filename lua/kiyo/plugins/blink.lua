d5ccaebea436c26930b23effe2dc16135f68fa58
gaurav2361 <gaurav23617@gmail.com>
2025-12-18 20:11:18 +0530

    feat: blink config changed

--- lua/kiyo/plugins/blink.lua
+++ lua/kiyo/plugins/blink.lua
@@ -14,10 +14,50 @@ return {
   ---@module 'blink.cmp'
   ---@type blink.cmp.Config
   opts = {
-    keymap = { preset = "default" },
+    keymap = {
+      preset = "enter",
+      ["<C-J>"] = { "select_next", "snippet_forward", "fallback" },
+      ["<C-K>"] = { "select_prev", "snippet_backward", "fallback" },
+      ["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
+      ["<C-e>"] = { "hide", "fallback" },
+      ["<C-y>"] = { "select_and_accept" },
+    },
 
     appearance = {
       nerd_font_variant = "mono",
+      kind_icons = {
+        Copilot = " ",
+        Text = "󰉿",
+        Method = "󰊕",
+        Function = "󰊕",
+        Constructor = "󰒓",
+
+        Field = "󰜢",
+        Variable = "󰆦",
+        Property = "󰖷",
+
+        Class = "󱡠",
+        Interface = "󱡠",
+        Struct = "󱡠",
+        Module = "󰅩",
+
+        Unit = "󰪚",
+        Value = "󰦨",
+        Enum = "󰦨",
+        EnumMember = "󰦨",
+
+        Keyword = "󰻾",
+        Constant = "󰏿",
+
+        Snippet = "󱄽",
+        Color = "󰏘",
+        File = "󰈔",
+        Reference = "󰬲",
+        Folder = "󰉋",
+        Event = "󱐋",
+        Operator = "󰪚",
+        TypeParameter = "󰬛",
+      },
     },
 
     signature = {
@@ -25,18 +65,145 @@ return {
     },
 
     completion = {
+
+      list = {
+        selection = {
+          preselect = function(ctx)
+            return not require("blink.cmp").snippet_active({ direction = 1 })
+          end,
+          auto_insert = function(ctx)
+            return vim.bo.filetype ~= "markdown"
+          end,
+        },
+      },
+
+      accept = {
+        create_undo_point = true,
+        resolve_timeout_ms = 500,
+        auto_brackets = {
+          enabled = true,
+          default_brackets = { "(", ")" },
+          override_brackets_for_filetypes = {},
+          kind_resolution = {
+            enabled = true,
+            blocked_filetypes = { "typescriptreact", "javascriptreact", "vue" },
+          },
+          semantic_token_resolution = {
+            enabled = true,
+            blocked_filetypes = { "java" },
+            timeout_ms = 400,
+          },
+        },
+      },
+
       documentation = {
+        window = {
+          border = nil,
+          scrollbar = false,
+          winhighlight = "Normal:BlinkCmpDoc,FloatBorder:BlinkCmpDocBorder,EndOfBuffer:BlinkCmpDoc",
+        },
         auto_show = true,
         auto_show_delay_ms = 500,
       },
 
       menu = {
+        auto_show = true,
         draw = {
-          columns = { { "kind_icon" }, { "label", "label_description", gap = 1 } },
+          treesitter = {
+            "lsp",
+          },
+          columns = {
+            -- { "label", "label_detail", "label_description", gap = 1 },
+            { "kind_icon" },
+            { "label", "label_description", gap = 1 },
+            { "kind" },
+            { "source_name" },
+          },
+          components = {
+            kind_icon = {
+              ellipsis = false,
+              text = function(ctx)
+                return ctx.kind_icon .. ctx.icon_gap
+              end,
+              highlight = function(ctx)
+                return ctx.kind_hl
+              end,
+            },
+
+            kind = {
+              ellipsis = false,
+              width = { fill = true },
+              text = function(ctx)
+                return ctx.kind
+              end,
+              highlight = function(ctx)
+                return ctx.kind_hl
+              end,
+            },
+
+            label = {
+              width = { fill = true, max = 60 },
+              text = function(ctx)
+                return ctx.label .. ctx.label_detail
+              end,
+              highlight = function(ctx)
+                -- label and label details
+                local highlights = {
+                  {
+                    0,
+                    #ctx.label,
+                    group = ctx.deprecated and "BlinkCmpLabelDeprecated" or "BlinkCmpLabel",
+                  },
+                }
+                if ctx.label_detail then
+                  table.insert(highlights, {
+                    #ctx.label,
+                    #ctx.label + #ctx.label_detail,
+                    group = "BlinkCmpLabelDetail",
+                  })
+                end
+
+                -- characters matched on the label by the fuzzy matcher
+                for _, idx in ipairs(ctx.label_matched_indices) do
+                  table.insert(highlights, { idx, idx + 1, group = "BlinkCmpLabelMatch" })
+                end
+
+                return highlights
+              end,
+            },
+
+            label_description = {
+              width = { max = 30 },
+              text = function(ctx)
+                return ctx.label_description
+              end,
+              highlight = "BlinkCmpLabelDescription",
+            },
+
+            source_name = {
+              width = { max = 30 },
+              text = function(ctx)
+                return ctx.source_name
+              end,
+              highlight = "BlinkCmpSource",
+            },
+
+            source_id = {
+              width = { max = 30 },
+              text = function(ctx)
+                return ctx.source_id
+              end,
+              highlight = "BlinkCmpSource",
+            },
+          },
         },
       },
-    },
 
+      ghost_text = {
+        enabled = true,
+        show_with_selection = true,
+      },
+    },
     sources = {
       default = { "lsp", "path", "snippets", "buffer", "lazydev" },
       providers = {
