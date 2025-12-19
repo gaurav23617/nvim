62c5036e70bcd28ed84daaa3da988efe2b2207c0
gaurav2361 <gaurav23617@gmail.com>
2025-12-19 21:47:38 +0530

    feat: nix lsp formatter changed

---
+++ after/lsp/nixd.lua
@@ -0,0 +1,18 @@
+---@brief
+---
+--- https://github.com/nix-community/nixd
+---
+--- Nix language server, based on nix libraries.
+---
+--- If you are using Nix with Flakes support, run `nix profile install github:nix-community/nixd` to install.
+--- Check the repository README for more information.
+return {
+  root_markers = { "flake.nix", "default.nix", "shell.nix", "git" },
+  settings = {
+    nixd = {
+      formatting = {
+        command = { "nixfmt" },
+      },
+    },
+  },
+}
