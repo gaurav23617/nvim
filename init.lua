4732e4ef1e5397c30fe61198924be3878c4e5925
gaurav2361 <gaurav23617@gmail.com>
2025-12-19 19:36:38 +0530

    feat: nvim otions file created

--- init.lua
+++ init.lua
@@ -1,9 +1,2 @@
-vim.g.mapleader = " " -- change leader to a space
-vim.g.maplocalleader = " " -- change localleader to a space
-vim.opt.clipboard = "unnamedplus" -- allows neovim to access the system clipboard
-vim.opt.number = true -- set numbered lines
-vim.opt.relativenumber = true -- set relative numbered lines
-vim.opt.smartcase = true -- smart case
-vim.opt.smartindent = true -- make indenting smarter again
-
+require("kiyo.core.options")
 require("kiyo.core.lazy")
